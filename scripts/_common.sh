4851cab666c8a42c75703015f41d958168751d88
Félix Piédallu <felix@piedallu.me>
2022-02-19 23:53:05 +0100

    Upgrade to v10.8.0-alpha5

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,12 @@
 #=================================================
 
 debian=$(lsb_release --codename --short)
-pkg_version="10.7.7-1"
-version=$(echo "$pkg_version" | cut -d '-' -f 1)
+pkg_version="10.8.0-1"
+version=10.8.0-alpha5
+# version=$(echo "$pkg_version" | cut -d '-' -f 1)
 
-ffmpeg_pkg_version="4.3.2-1"
-ldap_pkg_version="12.0.0.0"
+ffmpeg_pkg_version="4.4.1-1"
+ldap_pkg_version="14.0.0.0"
 
 architecture=$(dpkg --print-architecture)
 
